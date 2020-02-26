7cc9e35c90b4e3077c960c3b3e220c014cde5bc6
Lucas Araujo e Silva <lucasaraujo8186@gmail.com>
2020-02-26 18:18:03 -0300

    arq javascript ES6

---
+++ main.js
@@ -0,0 +1,63 @@
+const vetor = [1, 2, 3, 4, 5, 6, 8];
+console.log(vetor);
+
+const newvetor = vetor.map(function(item, posicao) {
+  // soma o item do vetor mais sua posicao
+  return item + posicao;
+});
+
+console.log(newvetor);
+
+const soma = vetor.reduce(function(total, next) {
+  // reduce transforma o vetor em uma unica informacao
+  return total + next;
+});
+console.log(soma);
+
+const filter = vetor.filter(function(item) {
+  // Filtra o valor de acordo com a condicao do return (retornando true ou false)
+  return item % 2 === 0;
+});
+
+console.log(filter);
+
+const find = vetor.find(function(item) {
+  // procura uma informacao dentro do vetor
+  return item === 4;
+});
+
+console.log(find);
+
+const multi = vetor.map(item => item * 2); // ArrowFunction(recebendo um paramentro), faz as mesmas coisa que uma funcao anonima de maneira reduzida
+
+console.log(multi);
+
+const teste = () => ({ nome: "Lucas" }); // retornando um Objeto
+
+console.log(teste());
+
+const usuario = {
+  nome: "Lucas",
+  idade: 19,
+  endereco: {
+    cidade: "campinas",
+    estado: "SP"
+  }
+};
+
+const {
+  nome,
+  idade,
+  endereco: { cidade }
+} = usuario; // Desestruturacao de objetos
+console.log(nome);
+console.log(idade);
+console.log(cidade);
+
+function mostraNome({ nome, idade, endereco }) {
+  // Desestruturacao tambem funciona nos parametros dee funcao
+  console.log(nome, idade, endereco);
+}
+
+mostraNome(usuario);
+aaa;
